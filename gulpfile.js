336d57be878d8b1580c48d77c4535bb10411a24d
kayak <kayak@kayak.rocks>
2020-04-29 13:30:47 +1000

    feat: add pug compilation and injection
    resolves #6 and resolves #9

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,32 @@ const del = require('del')
 const gulpLoadPlugins = require('gulp-load-plugins')
 const plugins = gulpLoadPlugins()
 
-function minifyImages() {
-  return gulp
-    .src('src/img/*')
-    .pipe(plugins.imagemin())
-    .pipe(gulp.dest('out/img'))
+const clean = () => del(['out/**/*', 'tmp/**/*'])
+
+const minifyImages = () =>
+  gulp.src('src/img/*').pipe(plugins.imagemin()).pipe(gulp.dest('out/img'))
+
+const preprocessHTML = () => {
+  var target = gulp.src('src/pug/index.pug')
+  // It's not necessary to read the files (will speed up things), we're only after their paths:
+  var sources = gulp.src(['src/**/*.js', 'src/**/*.css'], { read: false })
+
+  return target.pipe(plugins.inject(sources)).pipe(gulp.dest('tmp'))
 }
 
-const clean = () => del(['out/**/*'])
+const compileHTML = () =>
+  // the .pug files in tmp/ have been preprocessed
+  gulp
+    .src('tmp/*.pug')
+    .pipe(plugins.pug({ verbose: true }))
+    .pipe(gulp.dest('out/'))
 
-exports.default = gulp.series(clean, minifyImages)
+const cleanTemp = () => del(['tmp/**/*'])
+
+exports.default = gulp.series(
+  clean,
+  minifyImages,
+  preprocessHTML,
+  compileHTML,
+  cleanTemp
+)
