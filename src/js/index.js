cebb66ae2b3778ebe38c78da25774d097d7115a9
kayak <kayak@kayak.rocks>
2020-04-30 14:59:53 +1000

    feat(anim): display first sprite
    🕺

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,30 @@
 import * as PIXI from 'pixi.js'
 
-let type = 'WebGL'
-if (!PIXI.utils.isWebGLSupported()) {
-  type = 'canvas'
-}
+let app = new PIXI.Application({ resizeTo: window })
 
-PIXI.utils.sayHello(type)
+// Add the canvas that Pixi automatically created for you to the HTML document
+document.body.appendChild(app.view)
+
+// make pixi full screen
+app.renderer.view.style.position = 'absolute'
+app.renderer.view.style.display = 'block'
+
+// load in alex
+const alex = PIXI.Sprite.from('img/alex_silhou.png')
+
+// the image is (intentionally) just a little bit too large. Let's scale him
+// down just a tad.
+// alex.setTransform(0, 0, 0.5, 0.5)
+
+// move his anchor point so we can place him correctly
+alex.anchor.set(0.5, 0.85)
+
+// place him
+alex.x = app.screen.width * 0.25
+alex.y = app.screen.height
+
+app.stage.addChild(alex)
+
+// TODO: add event listener to reshuffle sizes on window resize
+
+console.log('💀')
